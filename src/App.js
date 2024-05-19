247f6d5b91099052a48a78b5ad0e2a16d3151f76
dhinesh454 <dhinesh454@gmail.com>
2024-05-19 11:28:11 +0530

    Task5 HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import Main from './Main';
 import About from './navFiles/About';
+import Home from './navFiles/Home';
 
 
 const router = createBrowserRouter([
   {path:'/',element:<Main/>},
-  {path:'/about',element:<About/>}
+  {path:'/about',element:<About/>},
+  {path:'/home' , element:<Home/>}
 ]);
 
 function App() {
